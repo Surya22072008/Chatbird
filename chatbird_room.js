ee183fb7c48dca5381ed3e01cc9a73e44fa8d585
Surya22072008 <78208952+Surya22072008@users.noreply.github.com>
2021-06-17 14:43:15 +0530

    Add files via upload

--- chatbird_room.js
+++ chatbird_room.js
@@ -11,11 +11,27 @@ var firebaseConfig = {
     };
     // Initialize Firebase
     firebase.initializeApp(firebaseConfig);
-
+    user_name = localStorage.getItem("user_name");
+    function addRoom() {
+room_name = document.getElementById("room_name").value;
+firebase.database().ref("/").child(room_name).update({purpose : "Adding room name"});
+localStorage.setItem("room_name",room_name);
+window.location = "chatBird.html";
+}
+    
+document.getElementById("user_name").innerHTML = "Welcome " + user_name + "!";
 function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
        Room_names = childKey;
       //Start code
 
+console.log("Room name - "+ Room_names);
+row = "<div class='room_name' id="+Room_names+"onclick='redirectToRoomName(this.id)>#"+ Room_names+"</div><hr>";
+document.getElementById("output").innerHTML += row;
       //End code
       });});}
 getData();
+function redirectToRoomName(name) {
+      console.log(name);
+      localStorage.setItem("room_name",name);
+      window.location = "chatbird.html";
+}
